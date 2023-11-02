a625690a301a790df503535ff522cb5330ee9420
Prabhat Kumar <prabhssgg3@gmail.com>
2023-11-02 12:55:10 +0530

    structureReady

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>hello world</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
